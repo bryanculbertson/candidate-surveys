c2384ba8cf9b56ea4013f374bbb65db8b3562788
Bryan Culbertson <bryan.culbertson@gmail.com>
2024-11-10 12:39:44 -0800

    Refactor commands (#5)
    * Refactor commands
    
    * Fix numbering
    
    * Update readme

---
+++ survey/commands/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["generate_pdfs"]
+
+from .generate_pdfs import generate_pdfs
---
+++ survey/commands/generate_pdfs.py
@@ -0,0 +1,23 @@
+import csv
+
+from survey.conf_tools import load_conf
+from survey.pdf_tools import dump_questionnare_to_pdf
+
+
+def generate_pdfs(
+    csv_filename: str, config_filename: str, logo_directory: str, output_directory: str
+) -> None:
+    with open(csv_filename) as csvfile:
+        # Assumptions about the file:
+        #  * Tab delimited
+        #  * Proper '\n' delimiters for end-of-line
+        #  * The first line in the file contains column key names. These must match
+        #    the ALL_CAPS globals at the top of this file.
+        reader = csv.DictReader(csvfile, delimiter=",")
+        conf = load_conf(config_filename, list(reader.fieldnames or []))
+
+        conf["logo_directory"] = logo_directory.rstrip("/").strip()
+        conf["output_directory"] = output_directory.rstrip("/").strip()
+
+        for row in reader:
+            dump_questionnare_to_pdf(row, conf)
